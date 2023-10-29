ce308ede213b64e35fc17e9df125766098b66942
AnnmariyaFrancis <121210814+AnnmariyaFrancis@users.noreply.github.com>
2023-10-29 23:20:04 +0530

    Update Set_Matrix_Zeroes.py

--- Leetcode Questions/Matrix/Set_Matrix_Zeroes.py
+++ Leetcode Questions/Matrix/Set_Matrix_Zeroes.py
@@ -2,10 +2,27 @@
 
 You must do it in place."""
 
-
 class Solution(object):
     def setZeroes(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        m=len(matrix)
+        n=len(matrix[0])
+        rows=set()
+        cols =set()
+        for i in range(m):
+            for j in range(n):
+                 if matrix[i][j] == 0:
+                          rows.add(i)
+                          cols.add(j)
+        for row in rows:
+             for j in range(n):
+                      matrix[row][j] = 0
+
+        for col in cols:
+                for i in range(m):
+                      matrix[i][col] = 0
+sl=Solution()
+sl.setZeroes
