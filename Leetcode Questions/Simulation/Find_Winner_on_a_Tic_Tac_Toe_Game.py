412d543e436596fce61045a25821dd35f30718eb
AnnmariyaFrancis <121210814+AnnmariyaFrancis@users.noreply.github.com>
2023-10-30 01:06:32 +0530

    Update Find_Winner_on_a_Tic_Tac_Toe_Game.py

--- Leetcode Questions/Simulation/Find_Winner_on_a_Tic_Tac_Toe_Game.py
+++ Leetcode Questions/Simulation/Find_Winner_on_a_Tic_Tac_Toe_Game.py
@@ -9,3 +9,31 @@ No more moves can be played if the game is over.
 Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that the ith move will be played on grid[rowi][coli]. return the winner of the game if it exists (A or B). In case the game ends in a draw return "Draw". If there are still movements to play return "Pending".
 
 You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), the grid is initially empty, and A will play first."""
+class Solution(object):
+    def tictactoe(self, moves):
+        """
+        :type moves: List[List[int]]
+        :rtype: str
+        """
+        grid = [[' ' for _ in range(3)] for _ in range(3)]
+        player = 'A'
+        for i in moves:
+            row,col =i
+            grid[row][col] = player
+            if (
+                     (grid[row][0] == grid[row][1] == grid[row][2] == player) or
+                     (grid[0][col] == grid[1][col] == grid[2][col] == player) or 
+                     (row == col and grid[0][0] == grid[1][1] == grid[2][2] == player) or 
+                     (row + col == 2 and grid[0][2] == grid[1][1] == grid[2][0] == player) 
+            ):
+                      return player
+            if player == 'A':
+                     player= 'B' 
+            else:
+                    player='A'
+        if len(moves) == 9 :
+            return "Draw" 
+        else:
+            return "Pending"
+sl=Solution()
+sl.tictactoe
