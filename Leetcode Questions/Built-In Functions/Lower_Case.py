27da2b4d20102018c7a37363a03ff5c0da02a065
AnnmariyaFrancis <121210814+AnnmariyaFrancis@users.noreply.github.com>
2023-10-30 11:03:54 +0530

    Update Lower_Case.py

--- Leetcode Questions/Built-In Functions/Lower_Case.py
+++ Leetcode Questions/Built-In Functions/Lower_Case.py
@@ -1,1 +1,12 @@
-""""""
+"""Given a string s, return the string after replacing every uppercase letter with the same lowercase letter."""
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        for i in s:
+            return s.lower()
+
+s1=Solution()
+s1.toLowerCase
