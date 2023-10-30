fa7cf725d85a490a8dca0a2795c4756ae0ccba08
AnnmariyaFrancis <121210814+AnnmariyaFrancis@users.noreply.github.com>
2023-10-30 11:05:56 +0530

    Update Robot_Return_to_Origin.py

--- Leetcode Questions/Simulation/Robot_Return_to_Origin.py
+++ Leetcode Questions/Simulation/Robot_Return_to_Origin.py
@@ -3,3 +3,26 @@
 You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
 
 Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise."""
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        sum=0
+        list1=[]
+        for i in moves:
+               if i=="U":
+                    list1.append(U)
+               if i=="D":
+                     list1.append(-U)
+               if i=="L":
+                     list1.append(L)
+               if i=="R":
+                     list1.append(-L)
+        for i in list1:
+              sum=sum+i
+        if sum==0:
+              return True
+        else:
+              return False
